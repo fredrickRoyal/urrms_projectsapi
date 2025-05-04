9b49f0b7e1e7e4e189701aecb4f141a90ad3672f
fredrickroyal <kfredrick35@gmail.com>
2025-05-04 08:29:56 +0300

    initial code

---
+++ src/main/java/com/netthreads/urrms/projectsapi/ProjectsapiApplication.java
@@ -0,0 +1,13 @@
+package com.netthreads.urrms.projectsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netthreads/urrms/projectsapi/ProjectsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netthreads.urrms.projectsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
